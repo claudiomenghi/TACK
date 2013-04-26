246eaf7b6d233cf74a10ed9fbe19d4e9130c020b
marcellomaria.bersani <marcellomaria.bersani@a20f40c3-b3e2-553a-3ec1-038341743a14>
2013-04-26 14:07:04 +0000

    Added MILTIParserHandler class

---
+++ src/parserHandler/MITLIParserHandler.java
@@ -0,0 +1,42 @@
+package parserHandler;
+
+import java.util.Iterator;
+
+import delegateTranslator.*;
+import formulae.*;
+import formulae.MITLI.MITLIEventually_AtoB;
+import formulae.MITLI.MITLIFormula;
+
+public class MITLIParserHandler extends TLParserHandler {
+
+	@Override
+	public String getHeader(CLTLTranslatorEnum t) {
+	
+		String result = new String();
+		switch(t){
+			case AE2ZOT : {
+				result = result + new String("(asdf:operate 'asdf:load-op 'ae2zot) (use-package :trio-utils)\n");
+				
+				Iterator<Formula> f = super.iteratorOf();
+				
+				while (f.hasNext()){
+					MITLIFormula mitlf = (MITLIFormula) f.next();
+					result = result + new String("(define-tvar " + mitlf.z0() + " *real*)\n(define-tvar " + mitlf.z1() + " *real*)\n");
+					if (mitlf instanceof MITLIEventually_AtoB){
+						MITLIEventually_AtoB fm = (MITLIEventually_AtoB) mitlf;
+						int b = fm.upperbound();
+						int a = fm.lowerbound();
+						
+						int d = (int)Math.floor(b/(b-a)+1) + 1;  
+					
+						for (int i=0; i<d; i++)
+							result = result + new String("(define-tvar " + fm.x(i));
+					}
+				}				
+			};
+			case NUZOT: ;		
+		}
+		return result;
+	}
+
+}
