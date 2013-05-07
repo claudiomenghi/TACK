a334fb1658ce8dc94bf2a5a3eea1be0d4bc816f2
marcellomaria.bersani <marcellomaria.bersani@gmail.com>
2013-05-07 15:26:50 +0000

    little modification in auxiliaryConstr

--- src/formulae/MITLI/MITLIEventually_AtoB.java
+++ src/formulae/MITLI/MITLIEventually_AtoB.java
@@ -225,15 +225,18 @@ public class MITLIEventually_AtoB extends MITLIEventually implements Temporized{
 		
 		String[] _f1 = new String[d];
 		String[] _f2 = new String[((d*d) - d)/2];	
-		String[] _f3 = new String[d];
+		String[] _f3 = new String[d];		
 		String[] _f5 = new String[d];
 		String[] _f6 = new String[d];
+		String[] _f7 = new String[d];
+		String[][] __f7 = new String[d][d];
+		String[] _f8 = new String[d];
 		
 		for (int i=0; i<d; i++){
 			_f1[i] = t.rel("=", x(i,t), "0");
 			_f5[i] = t.rel(">=", x(i,t), "0");
 			for (int j=i+1; j<d; j++)
-				_f2[p++] = t.neg( t.and( t.rel("=", x(i,t), "0"), t.rel("=", x(j,t), "0") ) );
+					_f2[p++] = t.neg( t.and( t.rel("=", x(i,t), "0"), t.rel("=", x(j,t), "0") ) );
 			_f3[i] = t.implies(
 								t.rel("=", x(i,t), "0"),
 								t.X(
@@ -243,14 +246,32 @@ public class MITLIEventually_AtoB extends MITLIEventually implements Temporized{
 										)
 								)
 					);
+			
+			for (int j=0; j<d; j++)
+				if (j != (i+1)%d)
+					__f7[i][j] = t.rel(">", x(j,t), "0");
+			
+			_f7[i] = t.implies(
+					t.rel("=", x(i,t), "0"),
+					t.X(
+							t.R(
+									t.rel("=", x((i+1)%d,t), "0"),
+									t.and(__f7[i])									
+							)
+					)
+			);
 			_f6[i] = t.and(
-					t.G(
+							t.G(
+									t.or(
+											t.rel("=", t.X(x(i,t)), "0"),
+											t.rel(">", t.X(x(i,t)), x(i,t)))),
 							t.or(
-									t.rel("=", t.X(x(i,t)), "0"),
-									t.rel(">", t.X(x(i,t)), x(i,t)))),
-					t.or(
-							t.G(t.F(t.rel("=", x(i,t), "0"))), 
-							t.F(t.G(t.rel(">", x(i,t), String.valueOf(upperbound()))))));
+									t.G(t.F(t.rel("=", x(i,t), "0"))), 
+									t.F(t.G(t.rel(">", x(i,t), String.valueOf(upperbound()))))));
+			
+			if (i < d-1)
+				_f8[i] = t.rel("<", x((d-i)%d,t), x(d-(i+1),t));
+			
 		}
 		
 		//Formula (4)		
@@ -258,25 +279,32 @@ public class MITLIEventually_AtoB extends MITLIEventually implements Temporized{
 								t.or(high(t), low(t)), 
 								t.or(_f1)
 					);
-				
+
 			
 		// Formuala (5)
 		String f2 = t.and(_f2);
 		
 		
+		
 		// Formula (6)
 		String f3 = t.and(_f3);
-
+			
 		
 		String f4 = t.rel("=", x(0,t), "0");
 		
+		
 		// Positiveness of all clocks in the origin
 		String f5 = t.and(_f5);
-
-		// Clocks progression
+		
 		String f6 = t.and(_f6);
-	
-		return t.and(f4, f5, f6, t.G(t.and(f1,f2,f3)));
+		
+		// strict sequence among clocks
+		String f7 = t.and(_f7);
+		
+		// order of clocks at the origin
+		String f8 = t.and(_f8);
+		
+		return t.and(f4,f6,f8,t.G(t.and(f1,f2,f7)));
 					
 	}
 
