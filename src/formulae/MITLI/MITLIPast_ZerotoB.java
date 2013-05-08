faa0371148b92d480c0c264936eb466f55c388d4
marcellomaria.bersani <marcellomaria.bersani@gmail.com>
2013-05-08 17:38:38 +0000

    added zi>0

--- src/formulae/MITLI/MITLIPast_ZerotoB.java
+++ src/formulae/MITLI/MITLIPast_ZerotoB.java
@@ -80,16 +80,16 @@ public class MITLIPast_ZerotoB extends MITLIPast_AtoB{
 								t.U(
 										t.rel("<", subf.z0(t), "1"),
 										t.or(
-												t.and(subf.high(t), t.rel("<", subf.z0(t), "1")), 
+												t.and(subf.high(t), t.rel(">", subf.z0(t), "0"), t.rel("<", subf.z0(t), "1")), 
 												t.rel("=", subf.z0(t), "1"))											
 								)			
 					),
 					t.implies(
 							t.and(subf.low(t), t.rel("=", subf.z1(t), "0")),
 							t.U(
-									t.rel("<", subf.z0(t), "1"),
+									t.rel("<", subf.z1(t), "1"),
 									t.or(
-											t.and(subf.high(t), t.rel("<", subf.z0(t), "1")), 
+											t.and(subf.high(t), t.rel(">", subf.z1(t), "0"), t.rel("<", subf.z1(t), "1")), 
 											t.rel("=", subf.z0(t), "1"))											
 							)
 					)
