cb06b6758dcabfa1dc3ce7b80817cf2780694613
marcellomaria.bersani <marcellomaria.bersani@gmail.com>
2013-06-06 19:43:50 +0000

    Added interface SupplinfCountingClocks

---
+++ src/formulae/QTLI/SupplingCountingClocks.java
@@ -0,0 +1,19 @@
+package formulae.QTLI;
+
+import delegateTranslator.CLTLTranslator;
+
+public interface SupplingCountingClocks {
+
+	//return the string representing z^i 
+	public String z(int i);
+	
+	//returns the number of counting clocks needed by the formula implementing the interface
+	public int getCountingClocks();
+	
+	
+	//returns the CLTL formula constraining clocks
+	public String countingClocksConstraints(CLTLTranslator t);
+	
+	//returns the number of counting clocks needed by the formula implementing the interface
+	public void setCountingClocks(int nCountingClocks);
+}
