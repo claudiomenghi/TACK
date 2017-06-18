403a6f81215b05917505c6c6da8278988f072d84
claudiomenghi <cla.menghi@gmail.com>
2017-06-18 15:19:16 +0200

    adds the execution file

--- checker/src/main/java/tack/Tack.java
+++ checker/src/main/java/tack/Tack.java
@@ -1,4 +1,4 @@
-package tack.checker;
+package tack;
 
 import java.io.FileInputStream;
 import java.io.PrintStream;
@@ -17,15 +17,22 @@ import formulae.mitli.parser.MITLIParser;
 import ta.SystemDecl;
 import ta.parser.TALexer;
 import ta.parser.TAParser;
+import tack.checker.SystemChecker;
 
 public class Tack {
 
 	public static void main(String[] args) throws Exception {
-		Preconditions.checkArgument(args.length > 2,
+		PrintStream out = System.out;
+		out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		out.println("TACK  - Timed Automata ChecKer");
+		out.println("v. 0.0.1 - 18/06/2017\n");
+		out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		
+		Preconditions.checkArgument(args.length >= 2,
 				"you must specify the file that contains the MITLI formula and the bound to be used");
 
 		
-		PrintStream out = System.out;
+
 
 		String modelFile = args[0];
 		String propertyFile = args[1];		
@@ -33,36 +40,40 @@ public class Tack {
 		Preconditions.checkArgument(Files.exists(Paths.get(modelFile)), "The file: " + modelFile + " containing the model does not exist");
 		Preconditions.checkArgument(Files.exists(Paths.get(propertyFile)), "The file: " + propertyFile + " containing the property does not exist");
 	
-		out.println("TACK  - Timed Automata ChecKer");
-		out.println("v. 2.0 - 18/06/2017\n");
-		
+		out.println("Loading the property: "+propertyFile);	
 		// loading the property
-		String propertyFilePath = ClassLoader.getSystemResource(propertyFile).getPath();
-		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(propertyFilePath));
+		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(propertyFile));
 		MITLILexer lexer = new MITLILexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		MITLIParser parser = new MITLIParser(tokens);
 		parser.setBuildParseTree(true);
 		MITLIFormula formula = parser.mitli().formula;
 
+		out.println("Property loaded"+modelFile);
+		
+		
 		// loading the model
-		String modelFilePath = ClassLoader.getSystemResource(modelFile).getPath();
+		out.println("Loading the model");
 
-		ANTLRInputStream tainput = new ANTLRFileStream(
-				ClassLoader.getSystemResource(modelFilePath).getPath());
+		ANTLRInputStream tainput = new ANTLRFileStream(modelFile);
 		TALexer talexer = new TALexer(tainput);
 		CommonTokenStream tatokens = new CommonTokenStream(talexer);
 		TAParser taparser = new TAParser(tatokens);
 		taparser.setBuildParseTree(true);
 		SystemDecl system = taparser.ta().systemret;
 
+		out.println("Model loaded");
 		SystemChecker checker = new SystemChecker(system, formula, 10, System.out);
 		boolean result = checker.check();
 
-
+		out.println();
+		out.println();
+		out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		
 		out.println("The property of interest is : "+((result) ? "satisfied" : "not satisfied"));
 		out.println(((result) ? "" : "check the file output.hist.smt.txt to see the violating trace"));
 		out.println(((result) ? "" : "the mapping between the elements of the model and the used id can be found in the file elementsIDmap.txt"));
+		out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");
 		
 	}
 
