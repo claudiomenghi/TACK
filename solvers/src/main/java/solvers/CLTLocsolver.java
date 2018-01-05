90d0c5e591cf486d98215aee24e7e0f3d9226953
claudiomenghi <cla.menghi@gmail.com>
2018-01-05 08:09:25 +0100

    some changes in the solver

--- solvers/src/main/java/solvers/CLTLocsolver.java
+++ solvers/src/main/java/solvers/CLTLocsolver.java
@@ -1,7 +1,11 @@
 package solvers;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.base.Stopwatch;
@@ -19,16 +23,14 @@ public class CLTLocsolver {
 	private final PrintStream out;
 	private final int bound;
 
-
 	private float checkingtime;
-	
+
 	private double checkingspace;
-	
+
 	private long cltloc2zottime;
-	
+
 	private long sattime;
-	
-	
+
 	public CLTLocsolver(CLTLocFormula formula, PrintStream out, int bound) {
 
 		this.formula = formula;
@@ -41,29 +43,38 @@ public class CLTLocsolver {
 		Stopwatch timer = Stopwatch.createUnstarted();
 		timer.start();
 		out.println("Converting the CLTLoc formula in zot");
-		
-		//String zotEncoding = new CLTLoc2Ae2sbvzot(bound).apply(formula);
-		
-		
-		String zotEncoding = new CLTLoc2zot(bound, formula.accept(new CLTLocGetMaxBound()), ZotPlugin.AE2SBVZOT).apply(formula);
+
+		// String zotEncoding = new CLTLoc2Ae2sbvzot(bound).apply(formula);
+
+		String workingDir = System.getProperty("user.dir");
+
+		File f = new File(workingDir + "solver.txt");
+		ZotPlugin p = null;
+		if (f.exists()) {
+			BufferedReader reader = new BufferedReader(new FileReader(f));
+			String solver = reader.readLine();
+			p = ZotPlugin.valueOf(solver);
+			reader.close();
+		} else {
+			p = ZotPlugin.AE2ZOT;
+		}
+
+		String zotEncoding = new CLTLoc2zot(bound, formula.accept(new CLTLocGetMaxBound()), p).apply(formula);
 		timer.stop();
-		cltloc2zottime=timer.elapsed(TimeUnit.MILLISECONDS);
-	
-		
-		
+		cltloc2zottime = timer.elapsed(TimeUnit.MILLISECONDS);
+
 		out.println("Formula converted in zot");
 		timer.reset();
 		timer.start();
-		
-		ZotRunner zotRunner=new ZotRunner(zotEncoding, out);
+
+		ZotRunner zotRunner = new ZotRunner(zotEncoding, out);
 		boolean sat = zotRunner.run();
-		
-		
+
 		timer.stop();
-		this.sattime=zotRunner.getSatTime();
-		this.checkingspace=zotRunner.getCheckingspace();
-		this.checkingtime=zotRunner.getCheckingtime();
-	
+		this.sattime = zotRunner.getSatTime();
+		this.checkingspace = zotRunner.getCheckingspace();
+		this.checkingtime = zotRunner.getCheckingtime();
+
 		return sat;
 	}
 
