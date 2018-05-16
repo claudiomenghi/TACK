e93feebeeef938ec882c8461af4caa1258b92029
mbersani <bersani@elet.polimi.it>
2018-05-16 09:47:46 +0200

    Native encoding of Release

---
+++ mitli/src/main/java/formulae/mitli/MITLIRelease.java
@@ -0,0 +1,68 @@
+package formulae.mitli;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.base.Preconditions;
+
+import formulae.BinaryFormula;
+import formulae.mitli.visitors.MITLIVisitor;
+
+public class MITLIRelease extends MITLIFormula implements BinaryFormula<MITLIFormula> {
+
+	private final MITLIFormula subformula1;
+	private final MITLIFormula subformula2;
+	
+	private final String operator = "R";
+
+	public MITLIRelease(MITLIFormula subformula1, MITLIFormula subformula2) {
+		super();
+		Preconditions.checkNotNull(subformula1, "The first subformula cannot be null");
+		Preconditions.checkNotNull(subformula2, "The second subformula cannot be null");
+		this.subformula1 = subformula1;
+		this.subformula2 = subformula2;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public MITLIFormula getLeftChild() {
+		return this.subformula1;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public MITLIFormula getRightChild() {
+		return this.subformula2;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public <T> T accept(MITLIVisitor<T> visitor) {
+		return visitor.visit(this);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return "(" + this.subformula1 + " " + operator + " " + this.subformula2 + ")";
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Set<MITLIFormula> getChildren() {
+		Set<MITLIFormula> formulae=new HashSet<>();
+		formulae.add(subformula1);
+		formulae.add(subformula2);
+		return formulae;
+	}
+}
