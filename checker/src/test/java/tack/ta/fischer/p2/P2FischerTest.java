b91a036c635f2c8df181ebc2acf1d0182f46d61d
claudiomenghi <cla.menghi@gmail.com>
2018-05-23 09:49:11 +0200

    adds additional tests

--- checker/src/test/java/tack/ta/fischer/p2/P2FischerTest.java
+++ checker/src/test/java/tack/ta/fischer/p2/P2FischerTest.java
@@ -53,6 +53,115 @@ public class P2FischerTest {
 
 	}
 	
+	@Test
+	public void test4() throws IOException, ZotException {
+	
+		String path = ClassLoader.getSystemResource("tack/ta/fischer/p2/fischer_input_02b.q").getPath();
+
+		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(path));
+		MITLILexer lexer = new MITLILexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		MITLIParser parser = new MITLIParser(tokens);
+		parser.setBuildParseTree(true);
+		MITLIFormula formula = parser.mitli().formula;
+
+		ANTLRInputStream tainput = new ANTLRFileStream(
+				ClassLoader.getSystemResource("tack/ta/fischer/p2/fischer_input_02.ta").getPath());
+		TALexer talexer = new TALexer(tainput);
+		CommonTokenStream tatokens = new CommonTokenStream(talexer);
+		TAParser taparser = new TAParser(tatokens);
+		taparser.setBuildParseTree(true);
+		SystemDecl system = taparser.ta().systemret;
+
+		SystemChecker checker = new SystemChecker(system, formula, 20, System.out);
+		boolean result = checker.check(null);
+
+		assertFalse(result);
+
+	}
+	
+	@Test
+	public void test5() throws IOException, ZotException {
+	
+		String path = ClassLoader.getSystemResource("tack/ta/fischer/p2/fischer_input_02c.q").getPath();
+
+		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(path));
+		MITLILexer lexer = new MITLILexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		MITLIParser parser = new MITLIParser(tokens);
+		parser.setBuildParseTree(true);
+		MITLIFormula formula = parser.mitli().formula;
+
+		ANTLRInputStream tainput = new ANTLRFileStream(
+				ClassLoader.getSystemResource("tack/ta/fischer/p2/fischer_input_02.ta").getPath());
+		TALexer talexer = new TALexer(tainput);
+		CommonTokenStream tatokens = new CommonTokenStream(talexer);
+		TAParser taparser = new TAParser(tatokens);
+		taparser.setBuildParseTree(true);
+		SystemDecl system = taparser.ta().systemret;
+
+		SystemChecker checker = new SystemChecker(system, formula, 20, System.out);
+		boolean result = checker.check(null);
+
+		assertFalse(result);
+
+	}
+	
+	@Test
+	public void test3() throws IOException, ZotException {
+	
+		String path = ClassLoader.getSystemResource("tack/ta/fischer/p2/fischer_input_02b.q").getPath();
+
+		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(path));
+		MITLILexer lexer = new MITLILexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		MITLIParser parser = new MITLIParser(tokens);
+		parser.setBuildParseTree(true);
+		MITLIFormula formula = parser.mitli().formula;
+
+		ANTLRInputStream tainput = new ANTLRFileStream(
+				ClassLoader.getSystemResource("tack/ta/fischer/p2/fischer_input_02.ta").getPath());
+		TALexer talexer = new TALexer(tainput);
+		CommonTokenStream tatokens = new CommonTokenStream(talexer);
+		TAParser taparser = new TAParser(tatokens);
+		taparser.setBuildParseTree(true);
+		SystemDecl system = taparser.ta().systemret;
+
+		SystemChecker checker = new SystemChecker(system, formula, 20, System.out);
+		boolean result = checker.check(null);
+
+		assertFalse(result);
+
+	}
+	
+	
+	@Test
+	public void test2() throws IOException, ZotException {
+	
+		String path = ClassLoader.getSystemResource("tack/ta/fischer/p2/fischer_input_02a.q").getPath();
+
+		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(path));
+		MITLILexer lexer = new MITLILexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		MITLIParser parser = new MITLIParser(tokens);
+		parser.setBuildParseTree(true);
+		MITLIFormula formula = parser.mitli().formula;
+
+		ANTLRInputStream tainput = new ANTLRFileStream(
+				ClassLoader.getSystemResource("tack/ta/fischer/p2/fischer_input_02.ta").getPath());
+		TALexer talexer = new TALexer(tainput);
+		CommonTokenStream tatokens = new CommonTokenStream(talexer);
+		TAParser taparser = new TAParser(tatokens);
+		taparser.setBuildParseTree(true);
+		SystemDecl system = taparser.ta().systemret;
+
+		SystemChecker checker = new SystemChecker(system, formula, 20, System.out);
+		boolean result = checker.check(null);
+
+		assertFalse(result);
+
+	}
+	
 	
 	
 	
